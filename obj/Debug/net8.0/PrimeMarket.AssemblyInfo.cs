a7231bbae2afb8754987bf424de780c27372ebfa
kaantamerurkun <urkun33@gmail.com>
2025-04-19 15:04:49 +0300

    edit profile must be changed
    v3

--- obj/Debug/net8.0/PrimeMarket.AssemblyInfo.cs
+++ obj/Debug/net8.0/PrimeMarket.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeMarket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8793c77a6bf4450e1c9f1d7f26af1505c1b77980")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93eb7061758674c280589d5994d8328604b847e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeMarket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeMarket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
