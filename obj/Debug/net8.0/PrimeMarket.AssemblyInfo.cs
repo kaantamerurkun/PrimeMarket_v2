201aa18aff5f67213a0b60447249a98e7040f02f
kaantamerurkun <urkun33@gmail.com>
2025-05-01 19:15:38 +0300

    New Ver
    New

--- obj/Debug/net8.0/PrimeMarket.AssemblyInfo.cs
+++ obj/Debug/net8.0/PrimeMarket.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeMarket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a036128c747b20e99d3d9753171bb3e553105224")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bfc5e2f52c4a304fa086c18ffafb452edfb3d50")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeMarket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeMarket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
